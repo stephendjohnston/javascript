dacc4564d777605050e5fd9f04abae3786796454
Stephen Johnston <52013605+stephendjohnston@users.noreply.github.com>
2023-05-02 14:01:06 -0300

    add The Office 1 Outed

---
+++ codewars/7_kyu/the_office_1_outed.js
@@ -0,0 +1,19 @@
+const outed = (meet, boss) => {
+  const names = Object.keys(meet);
+  const peopleInRoom = names.length;
+
+  const result = names.reduce((total, name) => {
+    if (name === boss) {
+      total += meet[name];
+    }
+
+    total += meet[name];
+    return total;
+  }, 0);
+
+  return result / peopleInRoom <= 5 ? "Get Out Now!" : "Nice Work Champ!"
+}
+
+console.log(outed({'tim':0, 'jim':2, 'randy':0, 'sandy':7, 'andy':0, 'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0}, 'laura'));  // 'Get Out Now!')
+console.log(outed({'tim':1, 'jim':3, 'randy':9, 'sandy':6, 'andy':7, 'katie':6, 'laura':9, 'saajid':9, 'alex':9, 'john':9, 'mr':8}, 'katie'));  // 'Nice Work Champ!')
+console.log(outed({'tim':2, 'jim':4, 'randy':0, 'sandy':5, 'andy':8, 'katie':6, 'laura':2, 'saajid':2, 'alex':3, 'john':2, 'mr':8}, 'john'));   // 'Get Out Now!')
